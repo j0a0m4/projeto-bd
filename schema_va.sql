34c3762a2ad7c02bf6b3d85e23603e7e2858f19e
j0a0m4 <joaomarcoslopes@id.uff.br>
2019-11-29 13:21:31 -0300

    aplicacao e not null constraint

--- schema_va.sql
+++ schema_va.sql
@@ -3,7 +3,7 @@ use va;
 
 CREATE TABLE vacina (
   id INT AUTO_INCREMENT,
-  nome VARCHAR(100),
+  nome VARCHAR(100) NOT NULL,
   validade TIMESTAMP DEFAULT DATE_ADD(CURDATE(), INTERVAL 1 YEAR),
   PRIMARY KEY(id),
   UNIQUE (nome)
@@ -11,16 +11,28 @@ CREATE TABLE vacina (
 
 CREATE TABLE usuario (
   id INT AUTO_INCREMENT,
-  nome VARCHAR(230),
-  CPF CHAR(11),
+  nome VARCHAR(230) NOT NULL,
+  CPF CHAR(11) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (CPF)
 )
 
 CREATE TABLE aplicador (
   id INT AUTO_INCREMENT,
-  nome VARCHAR(230),
-  CPF CHAR(11),
+  nome VARCHAR(230) NOT NULL,
+  CPF CHAR(11) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (CPF)
+)
+
+CREATE TABLE aplicacao (
+  id INT AUTO_INCREMENT,
+  id_vacina INT NOT NULL,
+  id_usuario INT NOT NULL,
+  id_aplicador INT NOT NULL,
+  data_aplicacao TIMESTAMP DEFAULT CURDATE(),
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_vacina) REFERENCES vacina(id),
+  FOREIGN KEY(id_usuario) REFERENCES usuario(id),
+  FOREIGN KEY(id_aplicador) REFERENCES aplicador(id)
 )
