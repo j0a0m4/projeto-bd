6b4f1a4b647605a888b751134ce652c7af88f143
Marina Cotta Rangel <48564377+MarinaCotta@users.noreply.github.com>
2019-12-01 21:28:19 -0300

    Create dados_vacina.sql

---
+++ dados_vacina.sql
@@ -0,0 +1,83 @@
+use va;
+
+INSERT INTO
+  vacina (
+    nome,
+    descricao,
+    validade
+  )
+VALUES (
+	'BCG',
+    'Protege contra tuberculose',
+    '05.01.2020'
+),
+(
+	'VIP/VOP', 
+    'Protege contra poliomielite',
+    '22.01.2020'
+),
+(
+	'Tetravalente',
+    'Protege contra tétano, difteria, coqueluche e meningite',
+    '31.07.2020'
+),
+(
+	'Tetra Viral'
+    'Protege contra sarampo, rubéola e caxumba',
+    '20.05.2020'
+),
+(
+	'Hepatite A', 
+    'Protege contra Hepatite A',
+    '20.07.2020'
+),
+(
+	'Hepatite B', 
+    'Protege contra Hepatite B',
+    '20.08.2020'
+),
+(
+	'Febre Amarela',
+    'Protege contra Febre Amarela',
+    '27.03.2020'
+),
+(
+	'Penta/DTP', 
+    'Protege contra Haemophilus influenzae tipo B, tosse convulsa, tétano, hepatite B e difteria',
+    '11.03.2020'
+),
+(
+	'Pneumocócica 10v',
+    'Protege contra pneumonia, meningite, otite',
+    '24.03.2020'
+),
+(
+	'Rotavírus Humano', 
+    'Protege contra rotavirose',
+    '23.05.2020'
+),
+(
+	'Meningocócica C',
+    'Protege contra meningite e meningococcemia',
+    '09.07.2020'
+),
+(
+	'Varicela',
+    'Protege contra catapora',
+    '10.08.2020'
+),
+(
+	'HPV', 
+    'Protege contra o vírus do papiloma humano',
+    '05.01.2020'
+),
+(
+	'Pneumocócica 23V', 
+    'Protege contra o pneumococo',
+    '08.05.2020'
+),
+(
+	'Influenza', 
+    'Protege contra o vírus influenza',
+    '18.03.2020'
+);
