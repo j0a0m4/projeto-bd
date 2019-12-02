42b985df9b74b0f9a761d348c2d7cd5b3962229d
Marina Cotta Rangel <48564377+MarinaCotta@users.noreply.github.com>
2019-12-01 21:29:53 -0300

    Update dados_vacina.sql

--- dados_vacina.sql
+++ dados_vacina.sql
@@ -7,77 +7,77 @@ INSERT INTO
     validade
   )
 VALUES (
-	'BCG',
+    'BCG',
     'Protege contra tuberculose',
     '05.01.2020'
 ),
 (
-	'VIP/VOP', 
+    'VIP/VOP', 
     'Protege contra poliomielite',
     '22.01.2020'
 ),
 (
-	'Tetravalente',
+    'Tetravalente',
     'Protege contra tétano, difteria, coqueluche e meningite',
     '31.07.2020'
 ),
 (
-	'Tetra Viral'
+    'Tetra Viral'
     'Protege contra sarampo, rubéola e caxumba',
     '20.05.2020'
 ),
 (
-	'Hepatite A', 
+    'Hepatite A', 
     'Protege contra Hepatite A',
     '20.07.2020'
 ),
 (
-	'Hepatite B', 
+    'Hepatite B', 
     'Protege contra Hepatite B',
     '20.08.2020'
 ),
 (
-	'Febre Amarela',
+    'Febre Amarela',
     'Protege contra Febre Amarela',
     '27.03.2020'
 ),
 (
-	'Penta/DTP', 
+    'Penta/DTP', 
     'Protege contra Haemophilus influenzae tipo B, tosse convulsa, tétano, hepatite B e difteria',
     '11.03.2020'
 ),
 (
-	'Pneumocócica 10v',
+    'Pneumocócica 10v',
     'Protege contra pneumonia, meningite, otite',
     '24.03.2020'
 ),
 (
-	'Rotavírus Humano', 
+    'Rotavírus Humano', 
     'Protege contra rotavirose',
     '23.05.2020'
 ),
 (
-	'Meningocócica C',
+    'Meningocócica C',
     'Protege contra meningite e meningococcemia',
     '09.07.2020'
 ),
 (
-	'Varicela',
+    'Varicela',
     'Protege contra catapora',
     '10.08.2020'
 ),
 (
-	'HPV', 
+    'HPV', 
     'Protege contra o vírus do papiloma humano',
     '05.01.2020'
 ),
 (
-	'Pneumocócica 23V', 
+    'Pneumocócica 23V', 
     'Protege contra o pneumococo',
     '08.05.2020'
 ),
 (
-	'Influenza', 
+    'Influenza', 
     'Protege contra o vírus influenza',
     '18.03.2020'
 );
